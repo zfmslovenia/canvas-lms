51a5927cfda24bba68e532dc2777911a35746b0d
Matej Stavanja <matej.stavanja@zfm.si>
2018-08-21 08:54:37 +0200

    add locale files

---
+++ public/javascripts/custom_moment_locales/sl.js
@@ -0,0 +1,19 @@
+require('moment/locale/de')
+const moment = require('moment')
+
+const data = moment.localeData('sl')
+
+data._monthsShort = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'Maj',
+  'Jun',
+  'Jul',
+  'Avg',
+  'Sep',
+  'Okt',
+  'Nov',
+  'Dec'
+]
